82687aeea61b33b171346d229c10a5ff0e15506d
LorezV <derkach.tashcher@bk.ru>
2021-07-03 12:32:38 +0500

    'temp'

--- hakaton/hall/views.py
+++ hakaton/hall/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  return render(request, 'base.html')
+
+def news(request):
+  return render(request, 'baseNews.html')
