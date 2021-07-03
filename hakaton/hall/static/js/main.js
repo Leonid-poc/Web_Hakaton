bbc4a095c92c1766f24069690610130ea774b94f
LorezV <derkach.tashcher@bk.ru>
2021-07-03 14:16:22 +0500

    'верстка_2'

--- hakaton/hall/static/js/main.js
+++ hakaton/hall/static/js/main.js
@@ -1,3 +1,8 @@
 $('.header-burger-menu').on('click', (e) => {
   $('.header-nav').toggleClass('active');
 });
+
+let q = document.getElementsByClassName('news-post-desc')
+for (i of q) {
+    i.innerHTML = i.innerHTML.slice(0, 364) + '...'
+}
